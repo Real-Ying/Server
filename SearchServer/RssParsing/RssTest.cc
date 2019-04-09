de947539c902a530a4949182f60622804e3c0a0d
Real-Ying <1650502423@qq.com>
2019-04-09 15:47:28 +0000

    just finish first

--- SearchServer/RssParsing/RssTest.cc
+++ SearchServer/RssParsing/RssTest.cc
@@ -1,8 +1,10 @@
 #include"tinyxml2.h"
 #include<iostream>
+#include<string>
 using std::cout;
 using std::endl;
-using namesapce tinyxml2;
+using std::string;
+using namespace tinyxml2;
 
 void test0(void)
 {
@@ -12,7 +14,30 @@ void test0(void)
     { 
         cout<<"xmlfile load fail"<<endl;
     }
-    
+    XMLElement * itemNode = doc.FirstChildElement("rss")    //通过递归遍历找到第一篇文章 用指针itemNode指向
+	   	        	->FirstChildElement("channel")
+		       		->FirstChildElement("item");	
+    if(itemNode)    
+    {   //如果指向的这篇文章存在 则去拿下面的兄弟节点 
+        XMLElement * titleNode = itemNode->FirstChildElement("title");
+	XMLElement * linkNode = itemNode->FirstChildElement("link");
+	XMLElement * descriptionNode = itemNode->FirstChildElement("description");
+        XMLElement * contentNode = itemNode->FirstChildElement("content:encoded");
+        
+	//拿到兄弟节点指针后就去获取里面的字符串
+        string title(titleNode->GetText());
+	string link(linkNode->GetText());
+	string description(descriptionNode->GetText());
+	string content(contentNode->GetText());
+
+	cout<<"title --> "<<title<<endl;
+	cout<<"link  -->"<<link<<endl;
+    } 
 }
 
+int main()
+{
+    test0();
+    return 0;
+}
 
