ab7f00294a072d60a14a1f10207a29a3c66f7714
Real-Ying <1650502423@qq.com>
2019-04-20 14:55:47 +0000

    WebPage.hpp

---
+++ SearchServer/include/WebPage.hpp
@@ -0,0 +1,62 @@
+#ifndef _WD_WEB_PAGE_H_
+#define _WD_WEB_PAGE_H_
+
+#include "Configuration.hpp"
+#include "WordSegmentation.hpp"
+
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+
+using std::string;
+using std::map;
+using std::vector;
+using std::set;
+
+namespace wd
+{
+
+
+class WebPage
+{       //判断两篇文档是否相等
+	friend bool operator == (const WebPage & lhs, const WebPage & rhs); 
+	//对文档按Docid进行排序
+	friend bool operator < (const WebPage & lhs, const WebPage & rhs);
+public:
+	const static int TOPK_NUMBER = 20;   //k
+
+        //构造函数 
+	WebPage(string & doc, Configuration & config, WordSegmentation & jieba);
+
+	//获取文档的docid
+	int getDocId()
+	{	return _docId;	}
+        //获取文档的词频统计map
+	map<string, int> & getWordsMap()
+	{	return _wordsMap;	}
+        //获取文档
+	string getDoc()
+	{	return _doc;	}
+
+private:
+	//对已格式化文档进行处理
+	void processDoc(const string & doc, Configuration & config, WordSegmentation & jieba);
+        //求取文档的topk词集
+	void calcTopK(vector<string> & wordsVec, int k, set<string> & stopWordList);
+
+private:
+	string _doc;
+	int    _docId;        //文档id
+	string _docTitle;     //文档标题
+	string _docUrl;		  //文档URL
+	string _docContent;	  //文档内容
+	string _docSummary;   //文档摘要，需要自动生成，不是确定的
+
+	vector<string>   _topWords;
+	map<string, int> _wordsMap; // 存储文档的所有词(去停用词之后)和该项词的词频
+};
+
+}// end of namespace wd
+
+#endif
