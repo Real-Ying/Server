2ea5a19fc9957161b548bd700178636d7f194b2d
Real-Ying <1650502423@qq.com>
2019-04-24 15:36:25 +0000

    Dictionary today

---
+++ SearchServer/Dictionary/Dictionary.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<fstream>
+#include<sstream>
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::istringstream;
+
+struct Record {
+	string word;
+	int freq;
+};
+
+class Dictionary {
+ public:
+  void read(const string& filename);
+  void store(const string& filename);
+
+ private:
+  vector<Record> _dict;  
+		 
+};
+
+void Dictionary::read(const string& filename) {
+	ifstream ifs(filename);
+    if(!ifs) {
+		cout << file open error <<endl;
+		return;
+	}	
+	string line;
+	while(getline(ifs,line)) {  //当获得的行内容不为空时
+		istringstream iss(line);  //把这行内容拿给字符串IO处理
+		string preword;  //未经处理的单词
+		while(iss >> preword) {
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
