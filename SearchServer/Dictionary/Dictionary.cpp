2d11404ee3bc64f27c3460a0367f36381d0c9582
Real-Ying <1650502423@qq.com>
2019-04-29 15:53:43 +0000

    Dictionary.cpp

--- SearchServer/Dictionary/Dictionary.cpp
+++ SearchServer/Dictionary/Dictionary.cpp
@@ -28,22 +28,24 @@ class Dictionary {
 };
 
 void Dictionary::read(const string& filename) {
-	ifstream ifs(filename);
-    if(!ifs) {
-		cout << file open error <<endl;
-		return;
-	}	
-	string line;
-	while(getline(ifs,line)) {  //逐行读取文件内容存入内存空间变量line
-		istringstream iss(line);  //以line创建字符串输入流
-		string preword;  //未经处理单词
-		while(iss >> preword) {  //字符串输入流中读取未经处理的单词，>> 不可读入不可见字符起到过滤空格作用
-		    string word = preprocessWord(preword);  //调单词预处理函数对未处理单词进行处理	
-		}
-	}
+  ifstream ifs(filename);
+  if (!ifs) {
+    cout << file open error <<endl;
+    return;
+  }	
+  string line;
+  while (getline(ifs,line)) {  //逐行读取文件内容存入内存空间变量line
+    istringstream iss(line);  //以line创建字符串输入流
+    string preword;  //未经处理单词
+    while (iss >> preword) {  //字符串输入流中读取未经处理的单词，>> 不可读入不可见字符起到过滤空格作用
+      string word = preprocessWord(preword);  //调单词预处理函数对未处理单词进行处理	
+    }
+  }
 }
 
+string Dictionary::preprocessWord(string& word) {
 
+}
 
 
 
