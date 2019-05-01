6cda51e675f324ac5fa71f8f25cfed05c40c7f16
Real-Ying <1650502423@qq.com>
2019-05-01 15:55:39 +0000

    start Dictionary

--- SearchServer/Dictionary/Dictionary.cpp
+++ SearchServer/Dictionary/Dictionary.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<fstream>
 #include<sstream>
+#include <algorithm>
 using std::cout;
 using std::endl;
 using std::vector;
@@ -37,26 +38,42 @@ void Dictionary::read(const string& filename) {
   while (getline(ifs,line)) {  //逐行读取文件内容存入内存空间变量line
     istringstream iss(line);  //以line创建字符串输入流
     string preword;  //未经处理单词
-    while (iss >> preword) {  //字符串输入流中读取未经处理的单词，>> 不可读入不可见字符起到过滤空格作用
+    while (iss >> preword) {  //字符串输入流中读取未经处理的单词，>> 不可读入不可见字符起到过滤空格作用，对每个word进行 预处理 和 统计
       string word = preprocessWord(preword);  //调单词预处理函数对未处理单词进行处理	
-    }
+      if (word != string())  //处理后的单词不是因为包含数字返回的空字符串则进行统计
+          statistic(word);
   }
 }
 
 string Dictionary::preprocessWord(string& word) {  //对单词进行预处理
   for(size_t idx = 0; idx != word.size(); ++idx) { //size_t是无符号的int
     if (isdigit(word[idx])) {  //单词中有数字字母存在则该位返回空字符串替代
-      return string();
+        return string();
     } 
     else if (isupper(word[idx]))  //字母大写转小写  
     {
-      word[idx] = tolower(word[idx]);
+        word[idx] = tolower(word[idx]);
     }
   }
   return word;  //返回处理后的单词
 }
 
-
+void Dictionary::statistic(const string& word)
+{
+  for(size_t idx = 0; idx != _dict.size(); ++idx) {
+    if (word == _dict[idx].word) { //如果能够查找到
+        ++_dict[idx].freq;
+        break;
+    }
+  }
+				
+    if (idx == _dict.size()) {//没有找到
+        Record record;
+	record.word = word;
+	record.freq = 1;
+	_dict.push_back(record);
+    }
+}
 
 
 
