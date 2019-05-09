7ebf109bd3a59a911391a6633c1504b0b99f9ab2
Real-Ying <1650502423@qq.com>
2019-05-09 08:33:56 +0000

    WebPage.hpp with Google cpp style

--- SearchServer/include/WebPage.hpp
+++ SearchServer/include/WebPage.hpp
@@ -18,43 +18,44 @@ namespace wd
 {
 
 
-class WebPage
-{       //判断两篇文档是否相等
-	friend bool operator == (const WebPage & lhs, const WebPage & rhs); 
-	//对文档按Docid进行排序
-	friend bool operator < (const WebPage & lhs, const WebPage & rhs);
-public:
-	const static int TOPK_NUMBER = 20;   //k
+class WebPage {     
+  //判断两篇文档是否相等
+  friend bool operator == (const WebPage & lhs, const WebPage & rhs); 
+  //对文档按Docid进行排序
+  friend bool operator < (const WebPage & lhs, const WebPage & rhs);
+ public:
+  const static int TOPK_NUMBER = 20;   //k
 
-        //构造函数 
-	WebPage(string & doc, Configuration & config, WordSegmentation & jieba);
+  //构造函数 
+  WebPage(string & doc, Configuration & config, WordSegmentation & jieba);
+	
+  int getDocId() {                    //获取文档的docid
+    return _docId;	
+  }
+  map<string, int> & getWordsMap() {  //获取文档的词频统计map
+    return _wordsMap;	
+  }
+  string getDoc() {	              //获取文档
+    return _doc;	
+  }
 
-	//获取文档的docid
-	int getDocId()
-	{	return _docId;	}
-        //获取文档的词频统计map
-	map<string, int> & getWordsMap()
-	{	return _wordsMap;	}
-        //获取文档
-	string getDoc()
-	{	return _doc;	}
+ private:
+  //对已格式化文档进行处理（经PageLibc处理）
+  void processDoc(const string & doc, Configuration & config, WordSegmentation & jieba);
+  //求取文档的topk词集
+  void calcTopK(vector<string> & wordsVec, int k, set<string> & stopWordList);
 
-private:
-	//对已格式化文档进行处理
-	void processDoc(const string & doc, Configuration & config, WordSegmentation & jieba);
-        //求取文档的topk词集
-	void calcTopK(vector<string> & wordsVec, int k, set<string> & stopWordList);
+ private:
+  string _doc;          //已格式化的文档
 
-private:
-	string _doc;
-	int    _docId;        //文档id
-	string _docTitle;     //文档标题
-	string _docUrl;		  //文档URL
-	string _docContent;	  //文档内容
-	string _docSummary;   //文档摘要，需要自动生成，不是确定的
+  int    _docId;        //文档id
+  string _docTitle;     //文档标题
+  string _docUrl;       //文档URL
+  string _docContent;	//文档内容
+  string _docSummary;   //文档摘要，需要自动生成，不是确定的
 
-	vector<string>   _topWords;
-	map<string, int> _wordsMap; // 存储文档的所有词(去停用词之后)和该项词的词频
+  map<string, int> _wordsMap; // 存储文档的所有词(去停用词之后)和该项词的词频
+  vector<string>   _topWords; //topk词集vector<"word1", "word2", "word3", ~, "wordn">
 };
 
 }// end of namespace wd
