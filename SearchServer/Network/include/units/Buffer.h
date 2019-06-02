376e80bad1fd7b60b5e5d078cb24af475a8223bc
Real-Ying <1650502423@qq.com>
2019-06-02 11:08:03 +0000

    Buffer.h

--- SearchServer/Network/include/units/Buffer.h
+++ SearchServer/Network/include/units/Buffer.h
@@ -11,25 +11,28 @@ namespace wd {
 
 class Buffer : Noncopyable {
  public:
-  typedef std::function<void()> Task;
-  Buffer(size_t size);
+  typedef std::function<void()> Task;  //由于Task要绑定线程 所以设为void
+  Buffer(size_t size);                 //构造 由于它的数据成员都能自己析构 所以可无析构
 
-  void push(Task task);
+  void push(Task task);                //对外的两个操作接口
   Task pop();
 
-  bool full();
+  bool full();                         //两个状态判断函数
   bool empty();
 
   void wakeup_empty();
   void set_flag(bool flag);
 
  private:
-  MutexLock          mutex_;
-  Condition          notFull_;
+  MutexLock          mutex_;      //互斥锁对象
+
+  Condition          notFull_;    //两个状态信号量
   Condition          notEmpty_;
-  size_t             size_;
-  std::queue<Task>   que_;
-  bool               flag_;
+
+  size_t             size_;       //Queue' size
+  std::queue<Task>   que_;        //Queue对象
+
+  bool               flag_;       //Buffer的状态信号量
 };
 
 }// end of namespace wd
