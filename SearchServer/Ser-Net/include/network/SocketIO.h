03e9c6b8fedcf331223774e8148ffead09c9db74
Real-Ying <1650502423@qq.com>
2019-06-09 11:11:40 +0000

    SocketIO.h

--- SearchServer/Ser-Net/include/network/SocketIO.h
+++ SearchServer/Ser-Net/include/network/SocketIO.h
@@ -1,29 +1,24 @@
- ///
- /// @file    SocketIO.h
- /// @author  lemon(haohb13@gmail.com)
- /// @date    2015-11-05 16:10:26
- ///
-
 #ifndef _WD_SOCKETIO_H
 #define _WD_SOCKETIO_H
 #include <stdio.h>
 
-namespace wd
-{
-class SocketIO
-{
-public:
-	SocketIO(int sockfd);
+namespace wd {
 
-	size_t readn(char * buf, size_t count);
-	size_t writen(const char * buf, size_t count);
-	size_t readline(char * buf, size_t max_len);
+class SocketIO {
+ public:
+  SocketIO(int sockfd);
+  //接收/发送文件时确认收发到count位才完成
+  size_t readn(char * buf, size_t count);         //封装::read(3)  同时解决了其粘包问题   
+  size_t writen(const char * buf, size_t count);  //封装::write(3) 解决粘包
+  //读取以/n结尾的一行
+  size_t readline(char * buf, size_t max_len);    //由recv_peek()和readn()实现
 
-private:
-	size_t recv_peek(char * buf, size_t count);
+ private:
+  //预读取
+  size_t recv_peek(char * buf, size_t count);     //封装recv()的第四个参数不为0的第三种预取模式
 
-private:
-	int sockfd_;
+ private:
+  int sockfd_;
 };
 
 
