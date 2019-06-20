631a0d6bf48762b8bd1921f89c0b7f71f717561a
Real-Ying <1650502423@qq.com>
2019-06-20 12:45:54 +0000

    TcpServer.h OK

--- SearchServer/Ser-Net/include/network/TcpServer.h
+++ SearchServer/Ser-Net/include/network/TcpServer.h
@@ -9,24 +9,25 @@ namespace wd {
 
 class TcpServer {
  public:
-  typedef TcpConnection::TcpConnectionCallback TcpServerCallback;
+  //回调函数标签，在每个有注册的类中都有 标签名不同 类型相同
+  typedef TcpConnection::TcpConnectionCallback TcpServerCallback;  
 
-  TcpServer(unsigned short port);
+  TcpServer(unsigned short port);                      //构造 两种
   TcpServer(const char * pIp, unsigned short port);
 
-  void start();
+  void start();                                        //启动 和 关闭
   void stop();
 
-  void setConnectionCallback(TcpServerCallback cb);
+  void setConnectionCallback(TcpServerCallback cb);    //三个注册
   void setMessageCallback(TcpServerCallback cb);
   void setCloseCallback(TcpServerCallback cb);
 
  private:
-  InetAddress inetAddr_;
-  Socket sockfd_;
-  EpollPoller epollfd_;
+  InetAddress inetAddr_;                               //socket口对象
+  Socket sockfd_;                                      //socket对象
+  EpollPoller epollfd_;                                //Epollpoller对象
 
-  TcpServerCallback onConnectionCb_;
+  TcpServerCallback onConnectionCb_;                   //三个回调暂存
   TcpServerCallback onMessageCb_;
   TcpServerCallback onCloseCb_;
 };
